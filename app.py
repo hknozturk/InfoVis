4f0fd40c4e983a1f8cf64efc8b97a2fd4b77c3d1
hknozturk <hkn.ozturk.94@gmail.com>
2020-05-05 22:41:49 +0200

    map and graph_with_slider components are moved to 'components' folder

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import pandas as pd
-from map import PlotlyMap
-from graph_with_slider import GraphWithSlider
+from components.map import PlotlyMap
+from components.graph_with_slider import GraphWithSlider
 import custom_dash_index
 from components.list import List
 
