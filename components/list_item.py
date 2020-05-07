2b788a19695928514a6f9f31b10ca3898be252ec
Faris Jameel <farisjameel@gmail.com>
2020-05-07 16:58:40 +0200

    Updated states dataframe path according to config file.

--- components/list_item.py
+++ components/list_item.py
@@ -1,10 +1,13 @@
 import dash_html_components as html
 import pandas as pd
+import json
 
-
+with open('config.json', 'r') as f:
+    config_path = json.load(f)
+    home_directory = config_path['home_directory']
 class ListItem:
     def __init__(self):
-        self.states = pd.read_csv('./datasets/state_names.csv', sep=';')
+        self.states = pd.read_csv(home_directory + 'state_names.csv', sep=';')
         self.generateItems()
 
     def generateItems(self):
