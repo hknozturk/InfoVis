1ee3252c18985c0e8ddeffba61ce44b1b8f0627f
Faris Jameel <farisjameel@gmail.com>
2020-05-18 14:03:14 +0200

    Added timeline barplot in filter callback.

--- app.py
+++ app.py
@@ -77,6 +77,7 @@ app.layout = html.Div(id="app-layout", children=[
 
 @app.callback(
     [Output('map', 'figure'),
+     Output('timeline_bar_plot', 'figure'),
      Output('list-component', 'children')],
     [Input('year-range-slider', 'value'),
      Input('map', 'selectedData'),
@@ -86,10 +87,12 @@ app.layout = html.Div(id="app-layout", children=[
 def update_figure(selected_years, selected_data, dark_mode, list_sort_value):
     dataProcessing.filter_data(selected_years)
     if selected_data is None:
-        return [plotlyMap.draw_map(dark_theme=dark_mode), listComponent.generateList(list_sort_value, dark_mode)]
+        return [plotlyMap.draw_map(dark_theme=dark_mode),barPlot.timeline_bar_plot(),
+                listComponent.generateList(list_sort_value, dark_mode)]
     else:
         state_ids = plotlyMap.retrieve_selected_states(selected_data)
-        return plotlyMap.draw_map(selected_states=state_ids, dark_theme=dark_mode), listComponent.generateList(list_sort_value, dark_mode)
+        return [plotlyMap.draw_map(selected_states=state_ids, dark_theme=dark_mode), barPlot.timeline_bar_plot(),\
+               listComponent.generateList(list_sort_value, dark_mode)]
 
 
 @app.callback(
